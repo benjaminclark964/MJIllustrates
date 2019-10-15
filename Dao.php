5ddae27072d8ad559def27a3d37d19bce03950cd
Benjamin <benjaminclark964@u.boisestate.edu>
2019-10-15 10:26:55 -0600

    database connnection and needed php files

---
+++ Dao.php
@@ -0,0 +1,15 @@
+<?php
+class Dao {
+  private $host = 'us-cdbr-iron-east-05.cleardb.net';
+  private $dbname = 'heroku_c0ae98134ba57d2';
+  private $username = 'becd588ff4db0c';
+  private $password = '1acdca85';
+  public function getConnection() {
+    try {
+       $connection = new PDO("mysql:host={$this->host};dbname={$this->dbname}", $this->username, $this->password);
+    } catch (Exception $e) {
+      echo print_r($e,1);
+    }
+    return $connection;
+  }
+}
