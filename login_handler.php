b4a4724347f084f9c2da286af5510947ebe5d942
Benjamin <benjaminclark964@u.boisestate.edu>
2019-10-26 10:48:31 -0600

    Updated login_handler to check for valid user and password

--- login_handler.php
+++ login_handler.php
@@ -6,8 +6,19 @@ $user = $_POST['Username'];
 $pass = $_POST['Password'];
 // $valid = $dao->isValidUser($username, $password);
 $valid = false;
-if ($user == validUser($user) && $pass == validPassword($pass)) {
+if (isset($_POST['Username'])) {
   $valid = true;
+  $sql = "SELECT * FROM user WHERE	username="'.$user.'" AND password = "'.$pass.'" limit 1";
+  
+  $result = mysql_query($sql);
+  
+  if(mysql_num_rows($result)==1){
+	  echo "Successful Login";
+	  exit;
+  } else {
+	  echo "Unsuccessful login";
+	  exit();	
+  }
 }
 if ($valid) {
    header("Location: http://index.html");
